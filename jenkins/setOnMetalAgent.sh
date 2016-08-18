233828e05e88d6a4b337a3ffa2274e1137d89480
Luz Cazares <dlux@users.noreply.github.com>
2016-08-18 17:24:06 -0500

    Update setOnMetalAgent.sh

--- jenkins/setOnMetalAgent.sh
+++ jenkins/setOnMetalAgent.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# Script installs all dependencies (problably to setup jenkins agent)
-# to create an OnMetal IO v2 rackspace product
+# Prepare to run qa-jenkins-onmetal ansible playbooks (On top of Jenkins Agent)
+# See also qa-jenkins-onmetal/jenkins/jenkins_agent_cloudinit_dependencies
 
 # Requires rackspace RPC credentials
 
@@ -9,31 +9,13 @@
 # {1} - raxusername
 # {2} - rax api key
 
-# Ensure script is run as root
-if [ "$EUID" -ne "0" ]; then
-  echo "$(date +"%F %T.%N") ERROR : This script must be run as root." >&2
-  exit 1
-fi
-
-apt-get -y update
-apt-get -y upgrade
-apt-get install -y vim curl wget git
-apt-get install -y --force-yes build-essential libssl-dev python-dev pkg-config libvirt-dev libxml2-dev libxslt1-dev libpq-dev
-curl -sk https://bootstrap.pypa.io/get-pip.py | python
-pip install ansible>=2.0
-pip install pyrax
-pip install libvirt-python
-
-curl https://ec4a542dbf90c03b9f75-b342aba65414ad802720b41e8159cf45.ssl.cf5.rackcdn.com/1.2/Linux/amd64/rack -o /usr/local/bin/rack
-chmod +x /usr/local/bin/rack
-
 if [ ! -f ~/.ssh/id_rsa ]; then
     ssh-keygen -t rsa -b 4096 -q -N '' -f ~/.ssh/id_rsa
 fi
 
-if [ ! -d qa-jenkins-onmetal ]; then
-    git clone https://github.com/osic/qa-jenkins-onmetal
-fi
+rm -R qa-jenkins-onmetal
+
+git clone https://github.com/osic/qa-jenkins-onmetal
 
 if [[ -z "${1}" || -z "${2}"  ]]; then
     raxusername="rax_username"
