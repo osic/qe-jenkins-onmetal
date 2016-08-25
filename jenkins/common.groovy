49f9c07f075a4874b1d576648f2d2a71597244bb
Castulo J. Martinez <castulo.martinez@intel.com>
2016-08-25 11:17:15 -0700

    Wait for the new agent to finish set up
    The CI launches a new VM to run the jobs on at the beginning of
    the pipeline. This VM has to finish som set up first. This
    commits adds a wait for the setup to complete before doing the
    workflow.

--- jenkins/common.groovy
+++ jenkins/common.groovy
@@ -3,16 +3,33 @@
 echo "Loading external common functions..."
 
 
-def wait_for_ping(host_ip) {
+// Function that waits up to 10 minutes for a new agent to finish running cloud-init
+// to perform its initial setup
+def wait_for_agent_setup() {
+    
+    timeout(10) {
+        waitUntil {
+            echo 'Waiting for cloud-init to finish'
+            def cloud_init = readFile('/var/log/cloud-init-output.log')
+            def matcher = cloud_init =~ 'Cloud-init .* finished'
+            matcher ? true : false
+        }
+        echo 'Cloud-init completed. VM Initialized.'
+    }
+
+}
+
+
+// Function that pings a host until it replies back or times out 
+def wait_for_ping(host_ip, timeout_sec) {
 
     echo "Waiting for the host with IP:${host_ip} to become online."
     def response
     def current_time
-    int timeout = 360
     def initial_time = sh returnStdout: true, script: 'date +%s'
     waitUntil {
         current_time = sh returnStdout: true, script: 'date +%s'
-        if (current_time.toInteger() - initial_time.toInteger() > timeout) {
+        if (current_time.toInteger() - initial_time.toInteger() > timeout.toInteger()) {
             error "The host did not respond to ping within the timeout of ${timeout} seconds"
         }
         response = sh returnStatus: true, script: "ping -q -c 1 ${host_ip}"
