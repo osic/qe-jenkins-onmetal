12e9261ab9c2f02dd95f4fc23de41b48c4521c0e
lamarwhitej <lamarwhitej@yahoo.com>
2016-10-18 21:22:13 -0500

    Update osa_onmetal.functions.groovy

--- jenkins/osa_onmetal.functions.groovy
+++ jenkins/osa_onmetal.functions.groovy
@@ -318,6 +318,7 @@ def aggregate_results(host_ip) {
     //Pull persistent, during, api, smoke results from onmetal to ES vm
     sh """
     scp -o StrictHostKeyChecking=no -r root@${host_ip}:/root/output/ /home/ubuntu/
+    scp -o StrictHostKeyChecking=no -r root@${host_ip}:/root/subunit/ /home/ubuntu/
     """
 }
 
