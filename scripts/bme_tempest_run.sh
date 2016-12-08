a0f5a42350d781904bf40179cd9a53f884659ac5
Dan Kolb <openstack@dankolb.net>
2016-12-08 15:57:46 -0600

    corrects pathing

--- scripts/bme_tempest_run.sh
+++ scripts/bme_tempest_run.sh
@@ -1,8 +1,8 @@
-if [[ -n $1 ]]; then
-	echo "usage: bme_tempest_run.sh {smoke|persistent}"
+if [[ -n $1 || -n $2 ]]; then
+	echo "usage: bme_tempest_run.sh {smoke|persistent} <DIR>"
 	exit
 fi
-cd /opt/tempest_untagged
+cd ${2} 
 pip install -r requirements.txt 
 testr init
 stream_id=$(cat .testrepository/next-stream)
