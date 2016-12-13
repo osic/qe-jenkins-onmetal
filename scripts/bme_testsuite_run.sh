2c1266e6eeeca6084995ad4298ebd4bb1f27f8d4
Dan Kolb <openstack@dankolb.net>
2016-12-13 09:26:21 -0600

    adds conditionals to tempest install

--- scripts/bme_testsuite_run.sh
+++ scripts/bme_testsuite_run.sh
@@ -2,10 +2,26 @@ if [[ -n $1 || -n $2 ]]; then
 	echo "usage: bme_testsuite_run.sh [smoke|persistent-{clean,verify}] [DIR]"
 	exit
 fi
-cd ${2}
-pip install -r requirements.txt 
+
+if [[ $1 = "smoke" ]]; then
+  # smoke requires tempest to be installed, which doesn't follow persistent tests
+	cd ${2}/tempest
+  keys="admin_password image_ref image_ref_alt uri uri_v3 public_network_id"
+	for key in $keys
+	do
+		a="${key} ="
+		sed -ir "s|${a}.*|${a}.*|g" ${2}/tempest/etc/tempest.conf
+		b=$(grep ""${a}"" ${2}/tempest/etc/tempest.conf.osa)
+		sed -ir "s|${a}|${b}|g" ${2}/tempest/etc/tempest.conf
+	done
+
+else
+	cd ${2}
+fi
+
+pip install -r requirements.txt
 testr init
 stream_id=$(cat .testrepository/next-stream)
 ostestr --no-slowest --regex ${1}
-mkdir -p subunit/${1} 
+mkdir -p subunit/${1}
 cp .testrepository/${stream_id} subunit/${1}.$(date +%s).results
